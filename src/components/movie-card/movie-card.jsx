78c47b12e4dc095ed31a6725bb3439380a2a1519
unknown <miguel.semedo.2@outlook.com>
2022-11-04 16:16:18 +0000

    Add Styling to the Movie Card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './movie-card.scss';
+
 export class MovieCard extends React.Component {
 	render() {
 		const { movie, onMovieClick } = this.props;
+
 		return (
-			<div
-				className="movie-card"
-				onClick={() => {
-					onMovieClick(movie);
-				}}
-			>
-				{movie.Title}
+			<div className="movie-card-view-container">
+				<div
+					className="movie-card-view-poster"
+					onClick={() => {
+						onMovieClick(movie);
+					}}
+				>
+					<img
+						className="movie-card-view-image"
+						src={movie.ImagePath}
+						crossorigin="anonymous"
+					/>
+					<div className="movie-card-view-title-container">
+						<h2 className="movie-card-view-title">{movie.Title}</h2>
+					</div>
+				</div>
 			</div>
 		);
 	}
