5515d5600f20594e86340b8014962af1f172ea1a
unknown <miguel.semedo.2@outlook.com>
2022-11-04 16:16:35 +0000

    Add Styling to the Movie View

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 
+import './movie-view.scss';
+
 export class MovieView extends React.Component {
 	render() {
 		const { movie, onBackClick } = this.props;
 
 		return (
-			<div className="movie-view">
-				<div className="movie-poster">
-					<img src={movie.ImagePath} />
+			<div className="movie-view-container">
+				<div className="movie-view-poster">
+					<img
+						className="movie-view-image"
+						src={movie.ImagePath}
+						crossorigin="anonymous"
+					/>
+					<div className="movie-view-info-container">
+						<div className="movie-view-title">
+							<span className="label"></span>
+							<span className="value">{movie.Title}</span>
+						</div>
+						<div className="movie-view-description">
+							<span className="label"></span>
+							<span className="value">{movie.Description}</span>
+						</div>
+						<button
+							className="movie-view-button"
+							onClick={() => {
+								onBackClick(null);
+							}}
+						>
+							Back
+						</button>
+					</div>
 				</div>
-				<div className="movie-title">
-					<span className="label">Title: </span>
-					<span className="value">{movie.Title}</span>
-				</div>
-				<div className="movie-description">
-					<span className="label">Description: </span>
-					<span className="value">{movie.Description}</span>
-				</div>
-				<button
-					onClick={() => {
-						onBackClick(null);
-					}}
-				>
-					Back
-				</button>
 			</div>
 		);
 	}
