9d9e62a24472c533d023c62c907b1573c1d261a9
unknown <miguel.semedo.2@outlook.com>
2022-11-07 17:08:45 +0000

    Register Functionality

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,20 +1,59 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
 import PropTypes from 'prop-types';
 
 import './registration-view.scss';
 
-export function RegistrationView(props) {
+export function RegistrationView() {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
-	const [email, setEmail] = useState('');
-	const [birthdate, setBirthDate] = useState('');
+	const [usernameErr, setUsernameErr] = useState('');
+	const [passwordErr, setPasswordErr] = useState('');
+
+	const validate = () => {
+		let isReq = true;
+		if (username === '') {
+			setUsernameErr('Username required');
+			isReq = false;
+		} else if (username.length < 5) {
+			setUsernameErr({
+				usernameErr: 'Username must be at least 5 characters long',
+			});
+			isReq = false;
+		}
+		if (!password) {
+			setPasswordErr('Password required');
+			isReq = false;
+		} else if (password.match(/[^0-9a-z]/i)) {
+			setPasswordErr('Password may only contain letters and digits');
+			isReq = false;
+		}
+		return isReq;
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(username, password, email, birthdate);
-		/* Send a request to the server for authentication */
-		/* then call props.onLoggedIn(username) */
-		props.onRegistration(username);
+		const isReq = validate();
+		if (isReq) {
+			axios
+				.post('https://mikeflix2.herokuapp.com/users', {
+					Username: username,
+					Password: password,
+				})
+				.then((response) => {
+					const data = response.data;
+					console.log(response.data);
+					alert('Registration successful, please login!');
+					window.open('/', '_self');
+				})
+				.catch((err) => {
+					console.log(err.response.data);
+					console.log(err.response.status);
+					console.log(err.response.headers);
+					alert('unable to register');
+				});
+		}
 	};
 
 	return (
@@ -27,7 +66,10 @@ export function RegistrationView(props) {
 						type="text"
 						value={username}
 						onChange={(e) => setUsername(e.target.value)}
+						placeholder="Username"
+						required
 					/>
+					{usernameErr && <p>{usernameErr}</p>}
 				</label>
 				<label className="input-container">
 					Password:
@@ -35,36 +77,26 @@ export function RegistrationView(props) {
 						type="password"
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
+						placeholder="Password"
+						required
 					/>
-				</label>
-				<label className="input-container">
-					Email:
-					<input
-						type="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</label>
-				<label className="input-container">
-					BirthDate:
-					<input
-						type="date"
-						value={birthdate}
-						onChange={(e) => setBirthDate(e.target.value)}
-					/>
+					{passwordErr && <p>{passwordErr}</p>}
 				</label>
 				<button
 					className="registration-button"
 					type="submit"
 					onClick={handleSubmit}
 				>
-					Submit
+					Register
 				</button>
 			</form>
 		</div>
 	);
 }
 
-RegistrationView.propTypes = {
-	onRegistration: PropTypes.func.isRequired,
-};
+// RegistrationView.propTypes = {
+// 	register: PropTypes.shape({
+// 		Username: PropTypes.string.isRequired,
+// 		Password: PropTypes.string.isRequired,
+// 	}),
+// };
