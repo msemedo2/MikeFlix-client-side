10f7812350d83fc7af25c6cd22860b125bbc34f9
unknown <miguel.semedo.2@outlook.com>
2022-11-08 15:54:36 +0000

    Update Movie  View

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,39 +1,105 @@
 import React from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
 
 import './movie-view.scss';
 
-export class MovieView extends React.Component {
-	render() {
-		const { movie, onBackClick } = this.props;
+export const MovieView = ({ user, movies }) => {
+	const { movieId } = useParams();
+	const navigate = useNavigate();
 
-		return (
-			<div className="movie-view-container">
-				<div className="movie-view-poster">
-					<img
-						className="movie-view-image"
-						src={movie.ImagePath}
-						crossorigin="anonymous"
-					/>
-					<div className="movie-view-info-container">
-						<div className="movie-view-title">
-							<span className="label"></span>
-							<span className="value">{movie.Title}</span>
+	let selectedMovie = {};
+
+	function waitingForMovies() {
+		if (!selectedMovie) {
+			waitingForMovies();
+		} else {
+			selectedMovie = movies.find((movie) => movie._id === movieId);
+		}
+	}
+	waitingForMovies();
+
+	function addToFavs(movies, user) {
+		let username = localStorage.getItem('user');
+		let token = localStorage.getItem('token');
+		console.log(movies);
+		console.log(token);
+		selectedMovie = movies.find((movie) => movie._id === movieId);
+
+		axios
+			.post(
+				`https://mikeflix2.herokuapp.com/users/${username}/movies/${selectedMovie._id}`,
+				{},
+				{ headers: { Authorization: `Bearer ${token}` } }
+			)
+			.then((response) => {
+				console.log(response.data);
+				alert(`${selectedMovie.Title} has been added from your list.`);
+			})
+			.catch(function (err) {
+				console.log(err.response.data);
+				console.log(err.response.status);
+				console.log(err.response.headers);
+			});
+	}
+
+	return (
+		<div>
+			{!selectedMovie ? null : (
+				<div className="movie-view-container">
+					<div className="movie-view-poster">
+						<img
+							className="movie-view-image"
+							src={selectedMovie.ImagePath}
+							crossOrigin="anonymous"
+						/>
+						<div className="movie-view-info-container">
+							<div className="movie-view-title">
+								<span className="label"></span>
+								<span className="value">{selectedMovie.Title}</span>
+							</div>
+							<div className="movie-view-description">
+								<span className="label"></span>
+								<span className="value">{selectedMovie.Description}</span>
+							</div>
+							<div>
+								<Link to={`/directors/${selectedMovie.Director.Name}`}>
+									<button>Director</button>
+								</Link>
+								<Link to={`/genres/${selectedMovie.Genre.Name}`}>
+									<button>Genre</button>
+								</Link>
+							</div>
+							<button
+								className="movie-view-button"
+								onClick={() => {
+									navigate(-1);
+								}}
+							>
+								Back
+							</button>
+							<button
+								onClick={() => {
+									addToFavs(movies, user);
+								}}
+							>
+								Add to favorites
+							</button>
+							{/* <button
+								onClick={() => {
+									addToFavs(movie, user);
+								}}
+							>
+								Remove from favorites
+							</button> */}
 						</div>
-						<div className="movie-view-description">
-							<span className="label"></span>
-							<span className="value">{movie.Description}</span>
-						</div>
-						<button
-							className="movie-view-button"
-							onClick={() => {
-								onBackClick(null);
-							}}
-						>
-							Back
-						</button>
 					</div>
 				</div>
-			</div>
-		);
-	}
-}
+			)}
+		</div>
+	);
+};
